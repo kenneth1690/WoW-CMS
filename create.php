8efd7085d6bcc4508ec6e25b011d2ab122ca9d25
Markuu <markowskyy@o2.pl>
2019-09-07 20:41:41 +0200

    Creating Account GET done

--- create.php
+++ create.php
@@ -113,8 +113,13 @@ function handlePress(evt){
 
 
 <div id="content-wrapper">
+    <?php
+        if(isset($_GET['action'])){
+            $action = htmlspecialchars($_GET['action']);
+        }else{
+            ?>
 	<div id="content-inner" class="wm-ui-generic-frame wm-ui-genericform wm-ui-two-side-page-left wm-ui-content-fontstyle wm-ui-right-border wm-ui-top-border">
-    	<form action="created.php" method="post">
+    	<form action="create.php?action=confirm" method="post">
         <table>
             <tbody><tr>
                 <td><span class="wm-ui-form-identifier">Account name</span></td>
@@ -175,6 +180,114 @@ function handlePress(evt){
         </div>
     	</form>
 	</div>
+    <?php
+    }
+    if($action == "confirm"){
+        ?>
+        <div id="content-inner" class="wm-ui-generic-frame wm-ui-genericform wm-ui-two-side-page-left wm-ui-content-fontstyle wm-ui-right-border wm-ui-top-border">
+    	<?php 
+		  $regconn = mysqli_connect($db_host, $db_username, $db_password, $auth_db_name, $db_port);
+		  
+		  if (isset($_POST['registerbtn'])) {
+			$username = $_POST['userID'];
+			$pass1 = $_POST['userPW'];
+			$pass2 = $_POST['userConfirmPW'];
+			$email = $_POST['userEmail'];
+			$checkbx = $_POST['userCM'];
+
+			$sql_u = "SELECT * FROM account WHERE username='$username'";
+			$sql_e = "SELECT * FROM account WHERE email='$email'";
+			$res_u = mysqli_query($regconn, $sql_u);
+			$res_e = mysqli_query($regconn, $sql_e);
+
+			if (mysqli_num_rows($res_u) > 0) {
+			  ?><span class="wm-ui-form-identifier">Account name already taken.<br><br>In a moment you will be taken back to the registration page.</span><?php
+			  header( "refresh:5;url=create.php" );
+			}else if(mysqli_num_rows($res_e) > 0){
+			  ?><span class="wm-ui-form-identifier">Email address already taken.<br><br>In a moment you will be taken back to the registration page.</span><?php
+			  header( "refresh:5;url=create.php" );
+			}else if(empty($checkbx)){
+			  ?><span class="wm-ui-form-identifier">You must agree to <a href="/policies/terms" target="_blank" class="wm-ui-hyper-custom-b">Terms of Service</a> and <a href="/policies/privacy" target="_blank" class="wm-ui-hyper-custom-b">Privacy Policy</a><br><br>In a moment you will be taken back to the registration page.</span><?php
+			  header( "refresh:5;url=create.php" );
+			}else{
+				if(strlen($username) < 4){
+					?><span class="wm-ui-form-identifier">Your Account name must be from 4 to 16 characters.<br><br>In a moment you will be taken back to the registration page.</span><?php
+					header( "refresh:5;url=create.php" );
+				} else if(strlen($username) > 16){
+					?><span class="wm-ui-form-identifier">Your Account name must be from 4 to 16 characters.<br><br>In a moment you will be taken back to the registration page.</span><?php
+					header( "refresh:5;url=create.php" );
+				} else if(preg_match('/[\'^£$%&*()}{@!;:.#~?><>,|=_+¬-]/', $username)){
+					?><span class="wm-ui-form-identifier">Your Account name must do not contain special characters.<br><br>In a moment you will be taken back to the registration page.</span><?php
+					header( "refresh:5;url=create.php" );
+				} else{
+					if (preg_match('/(\w)\1{2,}/', $username)) {
+						?><span class="wm-ui-form-identifier">Characters in your account name can not be repeated three times in a row.<br><br>In a moment you will be taken back to the registration page.</span><?php
+						header( "refresh:5;url=create.php" );
+					}else{
+						if(!($pass1==$pass2)){
+							?><span class="wm-ui-form-identifier">The passwords do not match.<br><br>In a moment you will be taken back to the registration page.</span><?php
+							header( "refresh:5;url=create.php" );
+						}else{
+							if(strlen($pass2) < 6){
+								?><span class="wm-ui-form-identifier">Your password must be from at least from 6 characters.<br><br>In a moment you will be taken back to the registration page.</span><?php
+								header( "refresh:5;url=create.php" );
+							}else{
+								if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+									?><span class="wm-ui-form-identifier">Invalid email address format.<br><br>In a moment you will be taken back to the registration page.</span><?php
+									header( "refresh:5;url=create.php" );
+								} else{
+								   $passfinal = sha1(strtoupper($username).':'.strtoupper($pass2));
+								   $query = "INSERT INTO account (username, sha_pass_hash, email, expansion) VALUES ('$username', '$passfinal', '$email', '2')";
+								   $results = mysqli_query($regconn, $query);
+
+								   $conn = mysqli_connect($db_host, $db_username, $db_password, $cms_db_name, $db_port);
+								   $sql3= "SELECT * FROM account WHERE username = '" . $username . "'";
+									$result3= mysqli_query($regconn,$sql3);
+									$user3 = mysqli_fetch_array($result3);
+
+								   $newtoken = md5($user3['email']);
+								   $yourmail = $user3['email'];
+								   $to = $yourmail;
+								   $subject = $sitename." | Email Activation";
+								   $message = "Hello, \nthank you ".$_SESSION['loggedin']." registering, now we invite you to email activation..\n\nIn order to do this, click link right there:\n".htmlentities("http://".$_SERVER['SERVER_NAME']."/ucp/mail.php?action=activate")."&".htmlentities("token=".$newtoken);
+								   $headers = "From: ".$sitename;
+
+								   $insertlog = mysqli_query($conn, "INSERT INTO logs_acc (`logger`, `logger_id`, `logdetails`, `logdate`) 
+								   VALUES ('".$username."', '".$user3['id']."', 'ACCOUNT: Created Account: `".$username."`', NOW());");
+								   mysqli_close($regconn);
+									?><span class="wm-ui-form-identifier">Account '<?php echo $username ?>' has been successfully created.<br><br>Now you can join to the WoW server.</span><?php
+									header( "refresh:5;url=index.php" );
+								} 
+							}
+						}
+					}
+				}
+			}
+		  }
+		  mysqli_close($regconn);
+		?>
+	</div>
+	<div id="content-inner" class="wm-ui-generic-frame wm-ui-genericform wm-ui-two-side-page-left wm-ui-content-fontstyle wm-ui-right-border wm-ui-top-border">
+    	
+    </div>
+    <?php
+    }elseif(isset($_GET['action'])){
+        ?>
+        <div id="content-inner" class="wm-ui-content-fontstyle wm-ui-generic-frame">
+        <div id="wm-error-page">
+        <center>
+            <p>
+                <font size="6">No action choosed</font>
+            </p>
+            <p>
+                <font size="5">You have not selected an action.</font>
+            </p> 
+        </center>
+        </div>
+        </div>
+        <?php
+    }
+    ?>
 </div>
 <script type="text/javascript">
 $(function() {
