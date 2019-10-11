1de3d972d2c065c076d2c23e8f4337e09afc229c
Markuu <markowskyy@o2.pl>
2019-10-12 01:46:36 +0200

    Footer

--- forum/readtopic.php
+++ forum/readtopic.php
@@ -453,7 +453,6 @@ $update = mysqli_query($conn, "UPDATE topics SET views = views + 1 WHERE categor
 </div>
 
 <div id="page-footer">
-	<a href="/policies/terms">Terms of Service</a> &nbsp; <a href="/policies/privacy">Privacy Policy</a> &nbsp; <a href="/policies/refund"> Refund Policy </a> &nbsp; <a href="#">Contact Us</a><br>
 	Copyright ][ <?php echo $sitename; ?> ][ 2019. All Rights Reserved.
 </div>
 
