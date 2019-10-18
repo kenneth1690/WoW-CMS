f3c993be11c23cfcd584a77c154c1cda1f699151
Markuu <markowskyy@o2.pl>
2019-10-18 20:59:28 +0200

    Update viewticket.php

--- ucp/viewticket.php
+++ ucp/viewticket.php
@@ -39,42 +39,44 @@ if(!isset($_SESSION["loggedin"]) || empty($_SESSION["loggedin"])){
 <link rel="stylesheet" href="/css/font-awesome.min.css">
 <link rel="stylesheet" href="/css/wm-contextmenu.css">
 <style>
-		#customers {
-			  border-collapse: collapse;
-			  width: 100%;
-			}
+#categories {
+  border-collapse: collapse;
+  width: 100%;
+}
 
-			#customers td, #customers th {
-			  border: 1px solid #ddd;
-			  background: #0f0f0f none repeat-x left;
-			  color: #c1b575;
-				border-bottom: 1px solid #1e1e1e;
-				border-left: 1px solid transparent;
-				border-right: 1px solid transparent;
-			  padding: 10px;
-			  font-size: 15px;
-			}
+#categories td, #categories th {
+  border: 1px solid #ddd;
+  background: #0f0f0f none repeat-x left;
+  color: #c1b575;
+    border-bottom: 1px solid #1e1e1e;
+    border-left: 1px solid transparent;
+    border-right: 1px solid transparent;
+  padding: 10px;
+  font-size: 15px;
+}
 
-			#customers tr:nth-child(even){background-color: #f2f2f2;}
+#categories tr:nth-child(even){background-color: #f2f2f2;}
 
-			#customers tr:hover {background-color: #ddd;}
+#categories tr:hover {background-color: #ddd;}
 
-			#customers th {
-			  padding-top: 6.5px;
-			  padding-bottom: 6.5px;
-			  text-align: left;
-			  background-color: #131313;
-			  color: #505050;
-			  box-shadow: -2px 2px 2px transparent;
-			  border-top-right-radius: 0px;
-				border-top-left-radius: 0px;
-				border-left: 1px solid transparent;
-				border-right: 1px solid transparent;
-				border: 1px solid #1e1e1e;
-				font-size: 15px;
-				vertical-align: text-top;
-			}
-		</style>
+#categories th {
+	word-break: break-all;
+  padding-top: 6.5px;
+  padding-bottom: 6.5px;
+  text-align: left;
+  background-color: #131313;
+  color: #505050;
+  box-shadow: -2px 2px 2px transparent;
+  border-top-right-radius: 0px;
+	border-top-left-radius: 0px;
+	border-left: 1px solid transparent;
+	border-right: 1px solid transparent;
+    border: 1px solid #1e1e1e;
+	font-size: 15px;
+	vertical-align: text-top;
+}
+
+</style>
 </head>
 <body>
 <div class="navigation-wrapper">
@@ -186,15 +188,246 @@ if(!isset($_SESSION["loggedin"]) || empty($_SESSION["loggedin"])){
             $gm= "SELECT * FROM account_access WHERE id = '" . $idcheck . "'";
             $resultgm = mysqli_query($checkacp,$gm);
             $rowsgm = mysqli_fetch_array($resultgm);
+			
+			$selectans = mysqli_query($conn, "SELECT * FROM ticket_answers WHERE ticket_id = $ticid ORDER BY id ASC");
             
             if(isset($ticid)){
                 $checktic = mysqli_query($conn, 'SELECT * FROM tickets WHERE id="'.$ticid.'"');
                 if(mysqli_num_rows($checktic)>0){
 					$rowtic = mysqli_fetch_array($checktic);
                     if($idcheck==$rowtic['author_id']){
-						?>
-						
-						<?php
+		
+						if (mysqli_num_rows($selectans) != 0) {
+							?>
+							<table id='categories'>
+							<th colspan='2'>Answers:</th></tr>
+							<?php
+							if (isset($_GET['page_no']) && $_GET['page_no']!="") {
+								$page_no = $_GET['page_no'];
+							}else{
+								$page_no = 1;
+							}
+							
+							$cmsconn = mysqli_connect($db_host, $db_username, $db_password, $cms_db_name, $db_port);
+							
+							$total_records_per_page = 6;
+							$offset = ($page_no-1) * $total_records_per_page;
+							$previous_page = $page_no - 1;
+							$next_page = $page_no + 1;
+							$adjacents = "2"; 
+
+							$result_count = mysqli_query($cmsconn,"SELECT COUNT(*) As total_records FROM `ticket_answers`");
+							$total_records = mysqli_fetch_array($result_count);
+							$total_records = $total_records['total_records'];
+							$total_no_of_pages = ceil($total_records / $total_records_per_page);
+							$second_last = $total_no_of_pages - 1; // total page minus 1
+							
+							$select3 = mysqli_query($conn, "SELECT * FROM ticket_answers WHERE ticket_id = $ticid LIMIT $offset, $total_records_per_page");
+							
+							while ($row = mysqli_fetch_assoc($select3)) {
+								if(isset($_SESSION["loggedin"])) {
+										$nick = $_SESSION["loggedin"];
+										$checkacp = mysqli_connect($db_host, $db_username, $db_password, $auth_db_name, $db_port);
+									}
+								
+								$test = $row['author'];
+								
+								$sqlauth= "SELECT * FROM account WHERE username = '" . $nick . "'";
+								$resultauth = mysqli_query($checkacp,$sqlauth);
+								$rowsauth = mysqli_fetch_array($resultauth);
+								
+								$sql= "SELECT * FROM account WHERE username = '" . $test . "'";
+								$result = mysqli_query($checkacp,$sql);
+								$rows = mysqli_fetch_array($result);
+								
+								$sqlauthor="SELECT * FROM account WHERE username = '" . $row['author'] . "'";
+								$resultauthor = mysqli_query($checkacp,$sqlauthor);
+								$rowsauthor = mysqli_fetch_array($resultauthor);
+									
+								$avatar = $rows['avatar'];
+								$ipcheck = $rows['last_ip'];
+								$idcheck = $rowsauth['id'];
+								$idgmcheck = $row['author_id'];
+								
+								$gmmy= "SELECT * FROM account_access WHERE id = '" . $idcheck . "'";
+								$resultgmmy = mysqli_query($checkacp,$gmmy);
+								$rowsgmmy = mysqli_fetch_array($resultgmmy);
+								
+								$gm= "SELECT * FROM account_access WHERE id = '" . $idgmcheck . "'";
+								$resultgm = mysqli_query($checkacp,$gm);
+								$rowsgm = mysqli_fetch_array($resultgm);
+								?>
+								<tr><th width='25%'><center>
+								<?php
+									if($rowsgm['gmlevel']==1){
+										?>
+										<font color="00ba0d">Moderator</font><br>
+										<?php
+									}elseif($rowsgm['gmlevel']==2){
+										?>
+										<font color="cf7c00">Administrator</font><br>
+										<?php
+									}elseif($rowsgm['gmlevel']==3){
+										?>
+										<font color="c70000">Head Admin</font><br>
+										<?php
+									}elseif($rowsgm['gmlevel']==4){
+										?>
+										<font color="9000b8">Owner</font><br>
+										<?php
+									}
+									
+									if($rowsauthor['posts']>=0 && $rowsauthor['posts']<50){
+										?>
+										<font color="ffffff"><?php echo $row['author']; ?></font>
+										<?php
+									}elseif($rowsauthor['posts']>=50 && $rowsauthor['posts']<100){
+										?>
+										<font color="#1df701"><?php echo $row['author']; ?></font>
+										<?php
+									}elseif($rowsauthor['posts']>=100 && $rowsauthor['posts']<250){
+										?>
+										<font color="006dd7"><?php echo $row['author']; ?></font>
+										<?php
+									}elseif($rowsauthor['posts']>=250 && $rowsauthor['posts']<500){
+										?>
+										<font color="9e34e7"><?php echo $row['author']; ?></font>
+										<?php
+									}elseif($rowsauthor['posts']>=500){
+										?><font color="f57b01"><?php echo $row['author']; ?></font><?php
+									}
+									?>
+								<br><img src="/uploads/avatars/<?php echo $avatar; ?>" width="100px" height="100px">
+								<?php
+								if($idcheck==$row['author_id']){
+								?>
+								<br>
+								<font color="#1df701">Author</font>
+								<?php
+								}
+								?>
+								</center></th>
+								<th><span style="float:right; text-align: right;">
+								Posted date: <?php echo $row['date_posted']; ?>
+								</span><br><?php echo $row['answer']; ?></th></tr>
+								<?php
+							}
+							?>
+							</table>
+							<div id="content-inner" class="wm-ui-content-fontstyle wm-ui-generic-frame">
+								<div id="wm-error-page">
+									<?php
+											if (isset($_SESSION['loggedin'])) {
+												$select = mysqli_query($conn, "SELECT * FROM tickets WHERE id = $ticid");
+												$row = mysqli_fetch_assoc($select);
+												
+												$checkacp = mysqli_connect($db_host, $db_username, $db_password, $auth_db_name, $db_port);
+												
+												$nick = $_SESSION["loggedin"];
+												
+												$sql= "SELECT * FROM account WHERE username = '" . $nick . "'";
+												$result = mysqli_query($checkacp,$sql);
+												$rows = mysqli_fetch_array($result);
+												
+												$idcheck = $rows['id'];
+												
+												$gm= "SELECT * FROM account_access WHERE id = '" . $idcheck . "'";
+												$resultgm = mysqli_query($checkacp,$gm);
+												$rowsgm = mysqli_fetch_array($resultgm);
+
+												if($row['status']=='0'){
+													echo "<form action='/forum/replyto/".$getcid."/".$getscid."/".$gettid."'>
+																<input type='submit' value='ANSWER' class='wm-ui-btn'/>
+															</form>";
+												}elseif($row['status']=='1'){
+													if($rowsgm['gmlevel']>0){
+														echo "<form action='/forum/replyto/".$getcid."/".$getscid."/".$gettid."'>
+																<input type='submit' value='ANSWER' class='wm-ui-btn'/>
+															</form>";
+														echo "The ticket is locked, but you still have permission to answer.";
+													}else{
+														echo "Ticket is locked, you can not answer.";
+													}
+												}
+												
+												mysqli_close($checkacp);
+											}
+										?>
+								</div>
+							</div>
+							<?php
+							$select2 = mysqli_query($conn, "SELECT * FROM ticket_answers WHERE ticket_id = $ticid");
+							if (mysqli_num_rows($selectans) > 6) {
+							?>
+							<div id="content-inner" class="wm-ui-content-fontstyle wm-ui-generic-frame">
+								<div id="wm-error-page">
+									<strong>Page <?php echo $page_no." of ".$total_no_of_pages; ?></strong><br>
+									<?php // if($page_no > 1){ echo "<li><a href='/forum/readtopic/$getcid/$getscid/$gettid&page_no=1'>First Page</a></li>"; } ?>
+									
+									<b><a <?php if($page_no > 1){ echo "href='/forum/readtopic/$getcid/$getscid/$gettid&page_no=$previous_page'"; } ?>>Previous&nbsp;&nbsp;</a></b>
+									   
+									<?php 
+									if ($total_no_of_pages <= 10){  	 
+										for ($counter = 1; $counter <= $total_no_of_pages; $counter++){
+											if ($counter == $page_no) {
+												echo "<b><u><a>&nbsp;&nbsp;$counter&nbsp;&nbsp;</a></u></b>";	
+											}else{
+												echo "<b><a href='/forum/readtopic/$getcid/$getscid/$gettid&page_no=$counter'>&nbsp;&nbsp;$counter&nbsp;&nbsp;</a></b>";
+											}
+										}
+									}elseif($total_no_of_pages > 10){
+										if($page_no <= 4) {			
+											for ($counter = 1; $counter < 8; $counter++){		 
+												if ($counter == $page_no) {
+													echo "<b><u><a>&nbsp;&nbsp;$counter&nbsp;&nbsp;</a></u></b>";	
+												}else{
+													echo "<b><a href='/forum/readtopic/$getcid/$getscid/$gettid&page_no=$counter'>&nbsp;&nbsp;$counter&nbsp;&nbsp;</a></b>";
+												}
+											}
+											echo "<b><a>...</a></b>";
+											echo "<b><a href='/forum/readtopic/$getcid/$getscid/$gettid&page_no=$second_last'>&nbsp;&nbsp;$second_last&nbsp;&nbsp;</a></b>";
+											echo "<b><a href='/forum/readtopic/$getcid/$getscid/$gettid&page_no=$total_no_of_pages'>&nbsp;&nbsp;$total_no_of_pages&nbsp;&nbsp;</a></b>";
+										}elseif($page_no > 4 && $page_no < $total_no_of_pages - 4) {		 
+											echo "<b><a href='/forum/readtopic/$getcid/$getscid/$gettid&page_no=1'>&nbsp;&nbsp;1&nbsp;&nbsp;</a></b>";
+											echo "<b><a href='/forum/readtopic/$getcid/$getscid/$gettid&page_no=2'>&nbsp;&nbsp;2&nbsp;&nbsp;</a></b>";
+											echo "<b><a>...</a></b>";
+											for ($counter = $page_no - $adjacents; $counter <= $page_no + $adjacents; $counter++) {			
+												if ($counter == $page_no) {
+													echo "<b><u><a>&nbsp;&nbsp;$counter&nbsp;&nbsp;</a></u></b>";	
+												}else{
+													echo "<b><a href='/forum/readtopic/$getcid/$getscid/$gettid&page_no=$counter'>&nbsp;&nbsp;$counter&nbsp;&nbsp;</a></b>";
+												}                  
+										   }
+										   echo "<b><a>...</a></b>";
+										   echo "<b><a href='/forum/readtopic/$getcid/$getscid/$gettid&page_no=$second_last'>&nbsp;&nbsp;$second_last&nbsp;&nbsp;</a></b>";
+										   echo "<b><a href='/forum/readtopic/$getcid/$getscid/$gettid&page_no=$total_no_of_pages'>&nbsp;&nbsp;$total_no_of_pages&nbsp;&nbsp;</a></b>";      
+										}else {
+											echo "<b><a href='/forum/readtopic/$getcid/$getscid/$gettid&page_no=1'>&nbsp;&nbsp;1&nbsp;&nbsp;</a></b>";
+											echo "<b><a href='/forum/readtopic/$getcid/$getscid/$gettid&page_no=2'>&nbsp;&nbsp;2&nbsp;&nbsp;</a></b>";
+											echo "<b><a>...</a></b>";
+
+											for ($counter = $total_no_of_pages - 6; $counter <= $total_no_of_pages; $counter++) {
+												if ($counter == $page_no) {
+													echo "<b><u><a>&nbsp;&nbsp;$counter&nbsp;&nbsp;</a></u></b>";	
+												}else{
+													echo "<b><a href='/forum/readtopic/$getcid/$getscid/$gettid&page_no=$counter'>&nbsp;&nbsp;$counter&nbsp;&nbsp;</a></b>";
+												}                   
+											}
+										}
+									}
+									?>
+							
+									<b><a <?php if($page_no < $total_no_of_pages) { echo "href='/forum/readtopic/$getcid/$getscid/$gettid&page_no=$next_page'"; } ?>>&nbsp;&nbsp;Next</a></b>
+									<?php
+									if($page_no < $total_no_of_pages){
+										echo "<b><a href='/forum/readtopic/$getcid/$getscid/$gettid&page_no=$total_no_of_pages'>&nbsp;&nbsp;Last</a></b>";
+									}
+									?>
+								</div>
+							</div>
+							<?php
+							}
+						}
 					}else{
 						?>
 						<div id="content-inner" class="wm-ui-content-fontstyle wm-ui-generic-frame">
