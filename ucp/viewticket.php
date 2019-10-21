855a02ea16f6e7dc6c940d8657639eb44e0d7a6e
Markuu <markowskyy@o2.pl>
2019-10-22 00:56:24 +0200

    UCP pagination fixed

--- ucp/viewticket.php
+++ ucp/viewticket.php
@@ -222,7 +222,7 @@ if(!isset($_SESSION["loggedin"]) || empty($_SESSION["loggedin"])){
 							$next_page = $page_no + 1;
 							$adjacents = "2"; 
 
-							$result_count = mysqli_query($cmsconn,"SELECT COUNT(*) As total_records FROM `ticket_answers`");
+							$result_count = mysqli_query($cmsconn,"SELECT COUNT(*) As total_records FROM `ticket_answers` WHERE ticket_id = $ticid");
 							$total_records = mysqli_fetch_array($result_count);
 							$total_records = $total_records['total_records'];
 							$total_no_of_pages = ceil($total_records / $total_records_per_page);
